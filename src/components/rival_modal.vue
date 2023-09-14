866309f909ab3cb18bb17ec32de30938fc7c079e
RafaelCTobar <rafaelctobar569@gmail.com>
2023-09-14 14:56:20 -0500

    become change

--- src/components/rival_modal.vue
+++ src/components/rival_modal.vue
@@ -9,7 +9,7 @@
 </script>
 <template>
     <div v-show="condition" class="absolute rounded-lg flex flex-col justify-center items-center shadow-lg bg-white w-auto p-5">
-      <h3 class="font-bold text-3xl text-center">{{content}} <span class="text-emerald-700">{{sub}}</span></h3>
+      <h3 class="font-bold text-3xl text-center">{{content}} <span class="text-red-700">{{sub}}</span></h3>
       <div class="flex gap-6">
         <slot/>
       </div>
