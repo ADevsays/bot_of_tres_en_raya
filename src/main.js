0407e20be2f91955db923f43d239bfd9dfb45e96
RafaelCTobar <rafaelctobar569@gmail.com>
2023-09-14 22:16:18 -0500

    Revert "DELETE THIS COMMIT"
    This reverts commit d4ad7d99cc46384f36833f8c47d89d60bb3d704b.

--- src/main.js
+++ src/main.js
@@ -9,8 +9,3 @@ const app = createApp(App);
 app.use(pinia);
 app.mount('#app');
 
-let swLocation = './services/sw.js';
-
-if(navigator.serviceWorker){
-    navigator.serviceWorker.register(swLocation);
-}
