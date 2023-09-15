254bfdeb04cf748192fbc12d8bfaf85db725ac1b
RafaelCTobar <rafaelctobar569@gmail.com>
2023-09-14 21:02:23 -0500

    add env variables for audio

--- src/helpers/playSounds.js
+++ src/helpers/playSounds.js
@@ -1,5 +1,5 @@
 export function playSounds(){
-    const urlAudio = './src/assets/audios/Sound_effect.mp3';
+    const urlAudio = import.meta.env.VITE_AUDIO_SRC
     const audio = new Audio(urlAudio);
     audio.play();
     audio.volume = 0.5;
